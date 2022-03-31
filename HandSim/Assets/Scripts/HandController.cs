55e12d85da0fea3e7a894bd4bb0556b75eb8f459
tcerqueira <tiago.cerqueira7777@gmail.com>
2022-03-31 22:41:17 +0100

    Joint references

--- HandSim/Assets/Scripts/HandController.cs
+++ HandSim/Assets/Scripts/HandController.cs
@@ -15,18 +15,36 @@ public class HandController : MonoBehaviour
         public IPEndPoint e;
     }
 
-    bool up = true;
-    GameObject wrist;
-    GameObject[] fingers = new GameObject[5];
+    public struct Finger
+    {
+        public Transform[] joints;
+    }
+
+    Transform wrist;
+    Finger[] fingers = new Finger[5];
+
     // Start is called before the first frame update
     void Start()
     {
-        wrist = GameObject.Find("/KnobbyHand/Arm/Wrist");
-        fingers[0] = GameObject.Find("Thumb/MetacarpophalangealJoint");
-        fingers[1] = GameObject.Find("Index/MetacarpophalangealJoint");
-        fingers[2] = GameObject.Find("Middle/MetacarpophalangealJoint");
-        fingers[3] = GameObject.Find("Ring/MetacarpophalangealJoint");
-        fingers[4] = GameObject.Find("Pinky/MetacarpophalangealJoint");
+        wrist = GameObject.Find("/KnobbyHand/Arm/Wrist").transform;
+        Transform[] fingerRoots = new Transform[] {
+            GameObject.Find("Thumb").transform,
+            GameObject.Find("Index").transform,
+            GameObject.Find("Middle").transform,
+            GameObject.Find("Ring").transform,
+            GameObject.Find("Pinky").transform,
+        };
+
+        for(int i = 0; i < 5; i++)
+        {
+            fingers[i].joints = new Transform[3];
+            fingers[i].joints[0] = fingerRoots[i].Find("MetacarpophalangealJoint");
+            fingers[i].joints[1] = fingers[i].joints[0].Find("Proximal/ProximalinterphalangealJoint");
+            fingers[i].joints[2] = fingers[i].joints[1].Find("Intermediate/DistalinterphalangealJoint");
+            if(!fingers[i].joints[0] || !fingers[i].joints[1] || !fingers[i].joints[2]) {
+                Debug.Log($"Error finding joints: Finger {i}");
+            }
+        }
 
         // Receive a message and write it to the console.
         IPEndPoint e = new IPEndPoint(IPAddress.Any, 5433);
@@ -43,27 +61,27 @@ public class HandController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(up)
-        {
-            transform.Rotate(Vector3.forward * 60 * Time.deltaTime);
-            foreach(GameObject finger in fingers)
-            {
-                finger.transform.Rotate(Vector3.right * 90 * Time.deltaTime);
-            }
-        }
-        else
-        {
-            transform.Rotate(Vector3.back * 60 * Time.deltaTime);
-            foreach(GameObject finger in fingers)
-            {
-                finger.transform.Rotate(Vector3.left * 90 * Time.deltaTime);
-            }
-        }
+        // if(up)
+        // {
+        //     transform.Rotate(Vector3.forward * 60 * Time.deltaTime);
+        //     foreach(GameObject finger in fingers)
+        //     {
+        //         finger.transform.Rotate(Vector3.right * 90 * Time.deltaTime);
+        //     }
+        // }
+        // else
+        // {
+        //     transform.Rotate(Vector3.back * 60 * Time.deltaTime);
+        //     foreach(GameObject finger in fingers)
+        //     {
+        //         finger.transform.Rotate(Vector3.left * 90 * Time.deltaTime);
+        //     }
+        // }
 
-        if(transform.eulerAngles.z >= 90 || transform.eulerAngles.z <= 0)
-        {
-            up = !up;
-        }
+        // if(transform.eulerAngles.z >= 90 || transform.eulerAngles.z <= 0)
+        // {
+        //     up = !up;
+        // }
     }
 
     private void OnReceive(IAsyncResult ar)
