4178ee3790ea3f3544de047f781d480202848501
tcerqueira <tiago.cerqueira7777@gmail.com>
2022-04-11 22:32:47 +0100

    Update test

--- HandSim/Assets/Tests/glovedata_test.py
+++ HandSim/Assets/Tests/glovedata_test.py
@@ -2,19 +2,20 @@ import socket
 import json
 
 
-serverAddressPort   = ("127.0.0.1", 5433)
-
 # Create a UDP socket at client side
-UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+serverAddressPort = ("127.0.0.1", 5433)
+with socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM) as UDPClientSocket:
 
-# Send to server using created UDP socket
-data = {
-    "wrist": {"x":0.0,"y":0.0,"z":1.0,"w":1.0},
-    "fingers":[{"joints":[{"x":1.0,"y":0.0,"z":0.0,"w":1.0},{"x":0.0,"y":0.0,"z":0.0,"w":0.0},{"x":0.0,"y":0.0,"z":0.0,"w":0.0}]},{"joints":[{"x":1.0,"y":0.0,"z":0.0,"w":1.0},{"x":0.0,"y":0.0,"z":0.0,"w":0.0},{"x":0.0,"y":0.0,"z":0.0,"w":0.0}]},{"joints":[{"x":1.0,"y":0.0,"z":0.0,"w":1.0},{"x":0.0,"y":0.0,"z":0.0,"w":0.0},{"x":0.0,"y":0.0,"z":0.0,"w":0.0}]},{"joints":[{"x":1.0,"y":0.0,"z":0.0,"w":1.0},{"x":0.0,"y":0.0,"z":0.0,"w":0.0},{"x":0.0,"y":0.0,"z":0.0,"w":0.0}]},{"joints":[{"x":1.0,"y":0.0,"z":0.0,"w":1.0},{"x":0.0,"y":0.0,"z":0.0,"w":0.0},{"x":0.0,"y":0.0,"z":0.0,"w":0.0}]}]
-}
+    # Send to server using created UDP socket
+    data = {
+        "wrist": {"x":0.0,"y":0.0,"z":1.0,"w":1.0},
+        "fingers":[{"joints":[{"x":1.0,"y":0.0,"z":0.0,"w":1.0},{"x":0.0,"y":0.0,"z":0.0,"w":0.0},{"x":0.0,"y":0.0,"z":0.0,"w":0.0}]},{"joints":[{"x":1.0,"y":0.0,"z":0.0,"w":1.0},{"x":0.0,"y":0.0,"z":0.0,"w":0.0},{"x":0.0,"y":0.0,"z":0.0,"w":0.0}]},{"joints":[{"x":1.0,"y":0.0,"z":0.0,"w":1.0},{"x":0.0,"y":0.0,"z":0.0,"w":0.0},{"x":0.0,"y":0.0,"z":0.0,"w":0.0}]},{"joints":[{"x":1.0,"y":0.0,"z":0.0,"w":1.0},{"x":0.0,"y":0.0,"z":0.0,"w":0.0},{"x":0.0,"y":0.0,"z":0.0,"w":0.0}]},{"joints":[{"x":1.0,"y":0.0,"z":0.0,"w":1.0},{"x":0.0,"y":0.0,"z":0.0,"w":0.0},{"x":0.0,"y":0.0,"z":0.0,"w":0.0}]}]
+    }
 
-while True:
-    w = float(input(""))
-    data["wrist"]["w"] = w
-    dataJSON = str.encode(json.dumps(data));
-    UDPClientSocket.sendto(dataJSON, serverAddressPort)
+    while True:
+        w = float(input(""))
+        data["wrist"]["w"] = w
+        dataJSON = str.encode(json.dumps(data));
+        UDPClientSocket.sendto(dataJSON, serverAddressPort)
+
+    UDPClientSocket.close()
