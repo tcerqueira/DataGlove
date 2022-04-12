24fb5880090046f54b0cef529f1659dd5d0d6ed4
tcerqueira <tiago.cerqueira7777@gmail.com>
2022-04-12 19:20:16 +0100

    Update Quaternion class to extend Eigen::Quaternion class

--- DataAcquisition/src/Quaternion.hpp
+++ DataAcquisition/src/Quaternion.hpp
@@ -1,26 +1,50 @@
 #pragma once
 #include "ArduinoJson.hpp"
+#include <Eigen/Geometry>
+#include <Eigen/Dense>
 
-struct Quaternion
+class Quaternion : public Eigen::Quaterniond
 {
-    float x, y, z, w;
+public:
+    Quaternion()
+    : Eigen::Quaterniond(1,0,0,0)
+    {
+    }
+
+    Quaternion(float x, float y, float z, float w)
+    : Eigen::Quaterniond(w,x,y,z)
+    {
+    }
+
+    // This constructor allows you to construct Quaternion from Eigen expressions
+    Quaternion(const Eigen::Quaterniond& other)
+    : Eigen::Quaterniond(other)
+    {
+    }
+
+    // This method allows you to assign Eigen expressions to Quaternion
+    Quaternion& operator=(const Eigen::Quaterniond& other)
+    {
+        this->Eigen::Quaterniond::operator=(other);
+        return *this;
+    }
 
     JsonObject serialize(JsonObject obj)
     {
-        obj["x"] = x;
-        obj["y"] = y;
-        obj["z"] = z;
-        obj["w"] = w;
+        obj["x"] = x();
+        obj["y"] = y();
+        obj["z"] = z();
+        obj["w"] = w();
         return obj;
     }
 
     JsonObject serialize(JsonArray parent)
     {
         JsonObject obj = parent.createNestedObject();
-        obj["x"] = x;
-        obj["y"] = y;
-        obj["z"] = z;
-        obj["w"] = w;
+        obj["x"] = x();
+        obj["y"] = y();
+        obj["z"] = z();
+        obj["w"] = w();
         return obj;
     }
 
@@ -28,10 +52,10 @@ struct Quaternion
     JsonObject serialize(StaticJsonDocument<Capacity> parent)
     {
         JsonObject obj = parent.createNestedObject();
-        obj["x"] = x;
-        obj["y"] = y;
-        obj["z"] = z;
-        obj["w"] = w;
+        obj["x"] = x();
+        obj["y"] = y();
+        obj["z"] = z();
+        obj["w"] = w();
         return obj;
     }
 };
