cd7c67ac8f4465a802e1128f59547288c164821f
tcerqueira <tiago.cerqueira7777@gmail.com>
2022-04-16 23:09:54 +0100

    Counter for debug

--- Connector/connector.py
+++ Connector/connector.py
@@ -7,10 +7,12 @@ serverAddressPort = ("127.0.0.1", 5433)
 with socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM) as UDPClientSocket:
     # Serial connection
     serialConn = serial.Serial('COM3', 115200)
+    count = 0;
 
     while True:
         data = serialConn.readline()
-        # print(data.decode("UTF-8"))
+        # print(f'{count}:\t{data.decode("UTF-8")}')
         UDPClientSocket.sendto(data, serverAddressPort)
+        count = count + 1
 
     serialConn.close()
