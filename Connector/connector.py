992068cbfde575ec5eb3a55b8440bd33464061e7
tcerqueira <tiago.cerqueira7777@gmail.com>
2022-04-18 17:18:48 +0100

    Connector script inputs port via cmd line

--- Connector/connector.py
+++ Connector/connector.py
@@ -1,12 +1,17 @@
 import socket
 import serial
+import sys
 
+if len(sys.argv) != 2:
+    exit(f"Usage: python connector.py <PORT>\nExample: python3 connector.py COM3")
+
+port = sys.argv[1];
 
 # Create a UDP socket at client side
 serverAddressPort = ("127.0.0.1", 5433)
 with socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM) as UDPClientSocket:
     # Serial connection
-    serialConn = serial.Serial('COM3', 115200)
+    serialConn = serial.Serial(port, 115200)
     count = 0;
 
     while True:
