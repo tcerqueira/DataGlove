e2c89061d72f0a310fce47558295a2703657792b
tcerqueira <tiago.cerqueira7777@gmail.com>
2022-05-19 16:53:01 +0100

    I2C mux tracks current channel

--- DataAcquisition/src/Drivers/I2CMux.hpp
+++ DataAcquisition/src/Drivers/I2CMux.hpp
@@ -8,18 +8,32 @@ class I2CMux
 public:
     I2CMux(uint8_t i2c_addr);
     void setChannel(uint8_t channel);
+    uint8_t getCurrent();
 
 private:
     uint8_t i2c_addr;
+    uint8_t current_channel = 0;
 };
 
 I2CMux::I2CMux(uint8_t i2c_addr)
-    : i2c_addr(i2c_addr) {}
+    : i2c_addr(i2c_addr)
+{
+    setChannel(current_channel);
+}
 
 void I2CMux::setChannel(uint8_t channel)
 {
+    if(current_channel == channel)
+        return;
+    
     Wire.beginTransmission(i2c_addr);
     Wire.write(1 << channel);
     Wire.endTransmission();
+
+    current_channel = channel;
 }
 
+uint8_t I2CMux::getCurrent()
+{
+    return current_channel;
+}
