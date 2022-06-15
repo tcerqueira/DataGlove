a12c1efaf03bf824eeb724ace8b6c82c7f0c4ff0
tcerqueira <tiago.cerqueira7777@gmail.com>
2022-06-15 21:48:08 +0100

    Fix i2c adresses and delete calibration offsets

--- DataAcquisition/src/calibration_main.cpp
+++ DataAcquisition/src/calibration_main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 #include "Utils/Utils.h"
-#include "Core/Calibration.h"
 #include "Drivers/Imu.h"
 #include "Drivers/I2CMux.hpp"
 
@@ -12,18 +11,18 @@ extern "C" uint32_t set_arm_clock(uint32_t frequency);
 
 // Mpu9250 object
 Imu imus[NUMIMUS] = {
-    Imu(&Wire, Imu::I2C_ADDR_PRIM, IMU0_GX_OFFSET, IMU0_GY_OFFSET, IMU0_GZ_OFFSET),
-    Imu(&Wire, Imu::I2C_ADDR_SEC, IMU1_GX_OFFSET, IMU1_GY_OFFSET, IMU1_GZ_OFFSET),
-    Imu(&Wire, Imu::I2C_ADDR_PRIM, IMU2_GX_OFFSET, IMU2_GY_OFFSET, IMU2_GZ_OFFSET),
-    Imu(&Wire, Imu::I2C_ADDR_SEC, IMU3_GX_OFFSET, IMU3_GY_OFFSET, IMU3_GZ_OFFSET),
-    Imu(&Wire, Imu::I2C_ADDR_PRIM, IMU4_GX_OFFSET, IMU4_GY_OFFSET, IMU4_GZ_OFFSET),
-    Imu(&Wire, Imu::I2C_ADDR_PRIM, IMU5_GX_OFFSET, IMU5_GY_OFFSET, IMU5_GZ_OFFSET),
-    Imu(&Wire, Imu::I2C_ADDR_SEC, IMU6_GX_OFFSET, IMU6_GY_OFFSET, IMU6_GZ_OFFSET),
-    Imu(&Wire, Imu::I2C_ADDR_PRIM, IMU7_GX_OFFSET, IMU7_GY_OFFSET, IMU7_GZ_OFFSET),
-    Imu(&Wire, Imu::I2C_ADDR_SEC, IMU8_GX_OFFSET, IMU8_GY_OFFSET, IMU8_GZ_OFFSET),
-    Imu(&Wire, Imu::I2C_ADDR_SEC, IMU9_GX_OFFSET, IMU9_GY_OFFSET, IMU9_GZ_OFFSET),
-    Imu(&Wire, Imu::I2C_ADDR_PRIM, IMU10_GX_OFFSET, IMU10_GY_OFFSET, IMU10_GZ_OFFSET),
-    Imu(&Wire, Imu::I2C_ADDR_SEC, IMU11_GX_OFFSET, IMU11_GY_OFFSET, IMU11_GZ_OFFSET)
+    Imu(&Wire, Imu::I2C_ADDR_PRIM),
+    Imu(&Wire, Imu::I2C_ADDR_SEC),
+    Imu(&Wire, Imu::I2C_ADDR_PRIM),
+    Imu(&Wire, Imu::I2C_ADDR_SEC),
+    Imu(&Wire, Imu::I2C_ADDR_PRIM),
+    Imu(&Wire, Imu::I2C_ADDR_SEC),
+    Imu(&Wire, Imu::I2C_ADDR_PRIM),
+    Imu(&Wire, Imu::I2C_ADDR_SEC),
+    Imu(&Wire, Imu::I2C_ADDR_PRIM),
+    Imu(&Wire, Imu::I2C_ADDR_SEC),
+    Imu(&Wire, Imu::I2C_ADDR_PRIM),
+    Imu(&Wire, Imu::I2C_ADDR_SEC)
 };
 uint8_t mux_map[NUMIMUS] = { 1,1,2,2,3,3,4,4,5,5,0,0 };
 uint8_t joint_map[NUMIMUS] = { 0,1,2,3,4,5,7,8,10,11,13,14 };
@@ -46,7 +45,10 @@ void setup()
     {
         tca9548a.setChannel(mux_map[i]);
         if(!imus[i].init())
+        {
             Serial.print("Error initializing communication with IMU");
+            Serial.println(i);
+        }
     }
 
     // Offline calibration
@@ -64,7 +66,7 @@ void loop()
 void offline_calibration(uint8_t i)
 {
     tca9548a.setChannel(mux_map[i]);
-    for(uint32_t j=0; j < 100000; j++)
+    for(uint32_t j=0; j < 15000; j++)
     {
         while(!imus[i].read());
 
